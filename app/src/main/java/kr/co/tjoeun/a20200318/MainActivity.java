559a63e4ff96685702babfcd1111b12a8e95d715
leam159 <61855478+leam159@users.noreply.github.com>
2020-03-18 09:38:32 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200318/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.co.tjoeun.a20200318;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
